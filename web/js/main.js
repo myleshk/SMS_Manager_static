2e090eaf893133dd4d347d61263af0a80a2a43cf
Myles Fong <i@myles.hk>
2017-02-25 17:12:08 +0800

    Rewrite using VueJs(Ongoing). Finish login part

--- web/js/main.js
+++ web/js/main.js
@@ -1,54 +1,199 @@
 /**
  * Created by myles on 24/1/2017.
  */
-$("#changeDevice .submit").click(function () {
-    var code = $("input#code").val();
+// initialize wilddog
+var appId = "telxt";
+var config = {
+    syncURL: "https://" + appId + ".wilddogio.com",
+    authDomain: appId + ".wilddog.com"
+};
+var error_code_dict;
+wilddog.initializeApp(config);
 
-    if (code) {
-        $.post('ctrl.php', {
-            action: 'validate_code',
-            code: code
-        }, function (response) {
-            if (response['success']) {
-                var uuid = response['uuid'];
-                reload_uuid();
-            } else {
-                $("#uuid").text("绑定码错误，请重试");
+wilddog.sync().ref('error_code').once("value").then(function (snapshot) {
+    error_code_dict = snapshot.val();
+}).catch(function (err) {
+    console.error(err);
+});
+
+var main_app = new Vue({
+    el: '#app-main',
+    data: {
+        logged_in: -1, // -1/0/1 means loading/not logged in/logged in, respectively
+        view: "login",
+        email: "",
+        pswd: "",
+        error: false,
+        error_text: "",
+        info: false,
+        info_text: ""
+    },
+    computed: {
+        login_active: function () {
+            return {
+                active: this.view == "login"
             }
-        });
-        $("input#code").val('');
+        },
+        register_active: function () {
+            return {
+                active: this.view == "register"
+            }
+        }
+    },
+    methods: {
+        register_view: function () {
+            hidePrompt();
+            if (this.view == "login") {
+                this.view = "register";
+            }
+        },
+        login_view: function () {
+            hidePrompt();
+            if (this.view == "register") {
+                this.view = "login";
+            }
+        },
+        login: function () {
+            hidePrompt();
+            login(this.email, this.pswd);
+        },
+        logout: function () {
+            hidePrompt();
+            logout();
+        },
+        register: function () {
+            hidePrompt();
+            register(this.email, this.pswd);
+        },
+        resetPassword: function () {
+            hidePrompt();
+            resetPassword(this.email);
+        }
     }
 });
 
-$(document).ready(function () {
-    reload_uuid();
-    load_messages();
-    load_email();
+// check current user
+var currentUser;
+wilddog.auth().onAuthStateChanged(function (user) {
+    currentUser = wilddog.auth().currentUser;
+    if (currentUser) {
+        if (currentUser.emailVerified) {
+            main_app.logged_in = 1;
+        } else {
+            main_app.logged_in = 0;
+            sendEmailVerification(currentUser);
+            showError(null, "您的账号未激活，我们刚刚发送了确认邮件，请验证邮箱后重试登录");
+            logout();
+        }
+    } else {
+        main_app.logged_in = 0;
+    }
+    console.info("currentUser", currentUser);
 });
 
-function log_out() {
-    // clear cookies
-    document.cookie.split(";").forEach(function (c) {
-        document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
-    });
-    // reload
-    location.reload();
-}
+var message_ref = wilddog.sync().ref('message');
+message_ref.on("value", function (snapshot) {
+    console.log(snapshot.val());
+});
 
 
-function load_email() {
-    $.post('ctrl.php', {
-        action: "my_email"
-    }, function (response) {
-        if (response['success']) {
-            var email = response['email'];
-            $("#email").text(email);
-        } else {
-            $("#email").text("获取账号错误，请新登录");
-        }
+function login(email, pswd) {
+//        main_app.logged_in = -1;// loading
+    wilddog.auth().signInWithEmailAndPassword(email, pswd)
+        .then(function () {
+            console.info("login success");
+        }).catch(function (err) {
+        console.log(err);
+        main_app.logged_in = 0;
+        showError(err.code, err.message);
     });
 }
 
+function logout() {
+    wilddog.auth().signOut().then(function () {
+        console.info("user sign out.");
+    });
+}
+
+function register(email, pswd) {
+//        main_app.logged_in = -1;
+    wilddog.auth().createUserWithEmailAndPassword(email, pswd)
+        .then(function (user) {
+            console.info("user created.", user);
+        }).catch(function (err) {
+        console.error(err);
+        main_app.logged_in = 0;
+        showError(err.code, err.message);
+    });
+}
+
+function resetPassword(email) {
+    wilddog.auth().sendPasswordResetEmail(email).then(function () {
+        // 发送成功
+        showInfo("密码重置邮件已经发送，请查收邮箱");
+    }).catch(function (err) {
+        // 发生错误
+        console.error(err);
+        showError(err.code, err.message);
+    });
+}
+
+function hidePrompt() {
+    main_app.info = false;
+    main_app.error = false;
+}
+
+function showInfo(info_text) {
+    main_app.info = true;
+    main_app.info_text = info_text;
+}
+
+function showError(error_code, error_text) {
+    main_app.error = true;
+    if (error_code_dict && error_code_dict[error_code]) {
+        main_app.error_text = error_code_dict[error_code]['cn'];
+    } else {
+        main_app.error_text = error_text;
+    }
+}
+
+function sendEmailVerification(user) {
+    user.sendEmailVerification()
+        .then(function (user) {
+            console.info("link email.", user);
+        })
+        .catch(function (err) {
+            console.info("link email failed.", err.code, err);
+        });
+}
+
+/**********************************************************************
+ * OLD
+ ***********************************************************************/
+/*$("#changeDevice .submit").click(function () {
+ var code = $("input#code").val();
+
+ if (code) {
+ $.post('ctrl.php', {
+ action: 'validate_code',
+ code: code
+ }, function (response) {
+ if (response['success']) {
+ var uuid = response['uuid'];
+ reload_uuid();
+ } else {
+ $("#uuid").text("绑定码错误，请重试");
+ }
+ });
+ $("input#code").val('');
+ }
+ });
+
+ $(document).ready(function () {
+ reload_uuid();
+ load_messages();
+ load_email();
+ });*/
 
 function reload_uuid() {
     $.post('ctrl.php', {
